e4e7f2f65e477fcae8060480bac4895bf31bcf31
unknown <Amitsq>
2022-07-14 16:28:38 +0530

    string leetcode question

---
+++ goal-parser.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string interpret(string command) {
+     
+        string result="";
+        
+        for(int i=0;i<command.size();i++){
+            
+            if(command[i]=='G'){
+                result.push_back('G');
+                
+            }else if(command[i]=='(' && command[i+1]==')'){
+                result.push_back('o');
+                i++;
+                
+            }else if(command[i]=='(' && command[i+1]=='a' && command[i+2]=='l'&& command[i+3]==')'){
+                result.push_back('a');
+                result.push_back('l');
+                i=i+3;
+            }
+                
+            
+        }
+        return result;
+        
+    }
+};
