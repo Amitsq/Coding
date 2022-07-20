7a5ef2cbd23490f294250f3839e336ba61ac30bd
unknown <Amitsq>
2022-07-20 19:45:57 +0530

    New Cp question

---
+++ Gpl.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t; 
+    cin>>t;
+    while(t--){
+
+     long long int N;
+     cin>>N;
+
+     string S;
+     cin>>S;
+    
+    int  Result=0;
+    int M=1;
+     for(int i=S.size()-1;i>=0;i--){
+
+         if(S[i]=='1'){
+
+             Result=Result+M;
+         }
+         M=M*2;
+     }
+
+
+   cout<<Result<<endl;
+
+    }
+}
